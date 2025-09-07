17170a6975b67195d2f966b385af1ccbb3d30009
Git-emir <kumar2002ayush@gmail.com>
2025-09-07 12:21:10 +0530

    Time: 0 ms (100%), Space: 45.4 MB (47.63%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.java
+++ 2058-concatenation-of-array/2058-concatenation-of-array.java
@@ -2,12 +2,8 @@ class Solution {
     public int[] getConcatenation(int[] nums) {
         int n = nums.length;
         int arr[] = new int[2*n];
-        for(int i =0;i<n;i++){
-            arr[i] = nums[i];
-        }
-        for(int i = 0;i<n;i++){
-            arr[i +n] = nums[i];
-       }
+       System.arraycopy(nums,0,arr,0,n);
+       System.arraycopy(nums,0,arr,n,n);
        return arr;
     }
 }
