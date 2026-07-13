83b5d3f3e9711072faab5951c8363e8ab9924865
Ayush kumar <kumar2002ayush@gmail.com>
2026-07-13 23:29:27 +0530

    Time: 5 ms (44.26%), Space: 96.5 MB (32.13%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,30 +1,73 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        Stack<Integer> st = new Stack<>();
-        ListNode temp = head;
-        while(temp != null){
-            st.push(temp.val);
-            temp = temp.next;
+        if (head == null || head.next == null)
+            return true;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
         }
-        temp = head;
-        while(temp != null){
-            if(temp.val != st.peek()){
+        ListNode newHead = reverse(slow.next);
+        ListNode first = head;
+        ListNode second = newHead;
+        while (second != null) {
+            if (first.val != second.val) {
+                reverse(newHead);
                 return false;
             }
-            else{
-                st.pop();
-            }
-            temp = temp.next;
-        }return true;
+            first = first.next;
+            second = second.next;
+
+        }
+        reverse(newHead);
+        return true;
+
+    }
+
+    public ListNode reverse(ListNode head) {
+
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+
+            ListNode next = curr.next;
+
+            curr.next = prev;
+
+            prev = curr;
+
+            curr = next;
+        }
+
+        return prev;
     }
 }
+// Stack<Integer> st = new Stack<>();
+//         ListNode temp = head;
+//         while(temp != null){
+//             st.push(temp.val);
+//             temp = temp.next;
+//         }
+//         temp = head;
+//         while(temp != null){
+//             if(temp.val != st.peek()){
+//                 return false;
+//             }
+//             else{
+//                 st.pop();
+//             }
+//             temp = temp.next;
+//         }return true;
