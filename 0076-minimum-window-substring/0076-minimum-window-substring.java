436870d881e2cc1b54a7320b95963f97d7b03ff5
Ayush kumar <kumar2002ayush@gmail.com>
2026-07-19 15:33:20 +0530

    Time: 16 ms (57.3%), Space: 47.2 MB (47.5%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -2,56 +2,50 @@ class Solution {
     public String minWindow(String s, String t) {
         int n = s.length();
         int m = t.length();
+        Map<Character,Integer> hm = new HashMap<>();
 
-        Map<Character, Integer> hm = new HashMap<>();
-
-        for (int i = 0; i < m; i++) {
+        for(int i = 0;i<m;i++){
             char ch = t.charAt(i);
-            if (hm.containsKey(ch)) {
-                hm.put(ch, hm.get(ch) + 1);
-            } else {
-                hm.put(ch, 1);
+            if(hm.containsKey(ch)){
+                hm.put(ch,hm.get(ch)+1);
+            }else{
+                hm.put(ch,1);
             }
         }
-
-        int left = 0;
-        int right = 0;
+        int right =0;
+        int left =0;
         int count = m;
+        int sindx = -1;
         int minlen = Integer.MAX_VALUE;
-        int startidx = -1;
-
-        while (right < n) {
+        while(right < n){
             char ch = s.charAt(right);
-            if (hm.containsKey(ch)) {
-                if (hm.get(ch) > 0) {
+            if(hm.containsKey(ch)){
+                if(hm.get(ch) > 0){
                     count--;
                 }
-                hm.put(ch, hm.get(ch) - 1);
+                hm.put(ch,hm.get(ch) -1);
             }
+            while(count == 0){
+                if(right - left +1 < minlen){
+                    minlen = right -left+1;
+                    sindx = left;
 
-            while (count == 0) {
-                if (right - left + 1 < minlen) {
-                    minlen = right - left + 1;
-                    startidx = left;
                 }
                 char lch = s.charAt(left);
-                if (hm.containsKey(lch)) {
-                    hm.put(lch, hm.get(lch) + 1);
-
-                    if (hm.get(lch) > 0) {
-                        count++;
+                if(hm.containsKey(lch)){
+                    hm.put(lch, hm.get(lch)+1);
+                    if(hm.get(lch) > 0){
+                        count = count +1;
                     }
                 }
+                
                 left++;
+                
 
             }
             right++;
-
-        }
-        
-        if (startidx == -1) {
+        }if(sindx == -1){
             return "";
-        }
-        return s.substring(startidx, startidx + minlen);
+        } return s.substring(sindx,sindx+minlen);
     }
 }
