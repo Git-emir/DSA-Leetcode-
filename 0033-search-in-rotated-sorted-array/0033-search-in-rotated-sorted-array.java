c894bb6d91a45c71a10fbf64c7bc955d6e733c98
Ayush kumar <kumar2002ayush@gmail.com>
2026-08-07 16:38:57 +0530

    Time: 0 ms (100%), Space: 43.8 MB (65.95%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -13,18 +13,19 @@ class Solution {
                 } else {
                     low = mid + 1;
                 }
-            }else{
+            }
+             else {
 
                 if (nums[mid] <= nums[high]) {
                     if (nums[mid] <= target && target <= nums[high]) {
                         low = mid + 1;
-                    } else {
+                    }
+                     else {
                         high = mid - 1;
                     }
                 }
             }
 
-            
         }
         return -1;
     }
