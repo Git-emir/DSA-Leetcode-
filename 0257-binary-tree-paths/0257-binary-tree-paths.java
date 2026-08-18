f2132f3d1b31cd4f6c197a02964939f744404e50
Ayush kumar <kumar2002ayush@gmail.com>
2026-08-18 13:13:28 +0530

    Time: 111 ms (12.54%), Space: 50.2 MB (9.51%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.java
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -1,44 +1,48 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> ans = new ArrayList<>();
-        if(root == null) return ans;
+        if (root == null)
+            return ans;
         String s = "";
         s += root.val;
-        if(root.left == null && root.right == null) ans.add(s);
-        if(root.left != null){
-            cal(root.left,ans,s);
+        if (root.left == null && root.right == null)
+            ans.add(s);
+        if (root.left != null) {
+            cal(root.left, ans, s);
         }
-        if(root.right != null){
-            cal(root.right,ans,s);
-        }return ans;
-        
+        if (root.right != null) {
+            cal(root.right, ans, s);
+        }
+        return ans;
+
     }
-    private void cal(TreeNode node,List<String> ans,String path){
+
+    private void cal(TreeNode node, List<String> ans, String path) {
         path = path + "->" + node.val;
-        if(node.left == null && node.right == null){
+        if (node.left == null && node.right == null) {
             ans.add(path);
         }
-        if(node.left != null ){
-            cal(node.left,ans,path);
+        if (node.left != null) {
+            cal(node.left, ans, path);
         }
-        if(node.right != null){
-            cal(node.right,ans,path);
+        if (node.right != null) {
+            cal(node.right, ans, path);
         }
-        }
-    
+    }
+
 }
