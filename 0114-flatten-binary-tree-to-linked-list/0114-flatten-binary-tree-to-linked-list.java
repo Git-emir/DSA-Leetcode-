9f151de61bddac0dd12b24e88b5bba0b61c63a6c
Ayush kumar <kumar2002ayush@gmail.com>
2026-08-21 17:10:20 +0530

    Time: 0 ms (100%), Space: 43.6 MB (93.98%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -14,19 +14,29 @@
  * }
  */
 class Solution {
+    TreeNode prev = null;
     public void flatten(TreeNode root) {
-        TreeNode cur = root;
-        while(cur != null){
-            if(cur.left != null){
-                TreeNode prev = cur.left;
-                while(prev.right != null){
-                    prev = prev.right;
-                }
-                prev.right = cur.right;
-                cur.right = cur.left;
-                cur.left = null;
-            }
-            cur = cur.right;
-        }
+        if(root == null) return;
+        flatten(root.right);
+        flatten(root.left);
+        root.right = prev;
+        root.left = null;
+        prev = root;
+        
     }
 }
+
+    //Morris approach TC-O(N) ,SC- O(1)
+    //  TreeNode cur = root;
+    //     while(cur != null){
+    //         if(cur.left != null){
+    //             TreeNode prev = cur.left;
+    //             while(prev.right != null){
+    //                 prev = prev.right;
+    //             }
+    //             prev.right = cur.right;
+    //             cur.right = cur.left;
+    //             cur.left = null;
+    //         }
+    //         cur = cur.right;
+    //     }
