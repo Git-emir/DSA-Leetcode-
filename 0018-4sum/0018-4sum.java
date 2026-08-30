b801bca5d9d3004d122f04aefb53576f6f8fe6f9
Ayush kumar <kumar2002ayush@gmail.com>
2026-08-30 22:02:13 +0530

    Time: 19 ms (83.21%), Space: 45.8 MB (46.86%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -3,19 +3,26 @@ class Solution {
         int n = nums.length;
         List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
-        for(int i = 0;i<n;i++){
-            if(i>0 && nums[i] == nums[i-1]) continue;
-            for(int j=i+1;j<n;j++){
-                if(j>i+1 && nums[j] == nums[j-1]) continue;
-                int l = j+1;
-                int r = n-1;
-                while(l<r){
-                    long sum = (long)nums[i];
+
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            for (int j = i + 1; j < n; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1])
+                    continue;
+
+                    
+                int l = j + 1;
+                int r = n - 1;
+
+                while (l < r) {
+                    long sum = (long) nums[i];
                     sum += nums[j];
                     sum += nums[l];
                     sum += nums[r];
 
-                    if(sum == target){
+                    if (sum == target) {
                         List<Integer> temp = new ArrayList<>();
                         temp.add(nums[i]);
                         temp.add(nums[j]);
@@ -23,15 +30,16 @@ class Solution {
                         temp.add(nums[r]);
 
                         ans.add(temp);
-                        l++;r--;
-                        while(l<r && nums[l] == nums[l-1]) l++;
-                        while(l<r && nums[r] == nums[r+1]) r--;
-                        
-                    }
-                    else if((sum < target)){
                         l++;
-                    }
-                    else{
+                        r--;
+                        while (l < r && nums[l] == nums[l - 1])
+                            l++;
+                        while (l < r && nums[r] == nums[r + 1])
+                            r--;
+
+                    } else if ((sum < target)) {
+                        l++;
+                    } else {
                         r--;
                     }
                 }
